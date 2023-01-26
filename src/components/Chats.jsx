b5fe019fde0258cd67fecb562a563e116be5c1fb
tanishqkrk <tanishqkrk@gmail.com>
2023-01-27 00:30:20 +0530

    minor updates

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -5,6 +5,18 @@ const Chats = () => {
                 <div className="userImgContainer"><img src="/user.svg" alt="" className="userImg" /></div>
                 <div className="userName">Niddhi</div>
             </div>
+            <div className="userChat">
+                <div className="userImgContainer"><img src="/user.svg" alt="" className="userImg" /></div>
+                <div className="userName">Maa</div>
+            </div>
+            <div className="userChat">
+                <div className="userImgContainer"><img src="/user.svg" alt="" className="userImg" /></div>
+                <div className="userName">Papa</div>
+            </div>
+            <div className="userChat">
+                <div className="userImgContainer"><img src="/user.svg" alt="" className="userImg" /></div>
+                <div className="userName">Archit</div>
+            </div>
         </div>
     )
 }
